f7faff9dde76ff645a1b3f8af243babd7718331c
Louis F. <me@itslouis.dev>
2025-04-18 20:32:58 +0200

    chore(dep): add eslint module & setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+
   future: {
     compatibilityVersion: 4
+  },
+
+  modules: ['@nuxt/eslint'],
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
   }
 })
