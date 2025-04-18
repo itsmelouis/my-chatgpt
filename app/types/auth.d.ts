a3189571d0203a4cc3ce133b592761f1bb5a0f15
Louis F. <me@itslouis.dev>
2025-04-18 22:05:15 +0200

    chore(dep): add `auth-utils` module & setup

---
+++ app/types/auth.d.ts
@@ -0,0 +1,13 @@
+declare module '#auth-utils' {
+  interface User {
+    id: string
+    name: string
+    email: string
+    avatar: string
+    username: string
+    provider: 'github'
+    providerId: number
+  }
+}
+
+export { }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+
+  modules: ['@nuxt/eslint', '@nuxt/ui', 'nuxt-auth-utils'],
   devtools: { enabled: true },
+
+  css: [
+    '~/assets/css/main.css'
+  ],
 
   future: {
     compatibilityVersion: 4
   },
-
-  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  compatibilityDate: '2024-11-01',
 
   eslint: {
     config: {
@@ -21,9 +25,5 @@ export default defineNuxtConfig({
   fonts: {
     priority: ['google', 'local'],
     provider: 'google'
-  },
-
-  css: [
-    '~/assets/css/main.css'
-  ],
+  }
 })
