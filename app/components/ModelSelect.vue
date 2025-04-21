ce43edd059565562da1a0604f59484c529aeba9d
Louis F. <me@itslouis.dev>
2025-04-21 21:34:55 +0200

    feat(Components): create `ModelSelect` component

---
+++ app/components/ModelSelect.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { model, models } = useLLM()
+</script>
+
+<template>
+  <USelectMenu
+    v-model="model"
+    :items="models"
+    variant="ghost"
+    :search-input="{
+      placeholder: 'Rechercher un modèle...',
+      icon: 'i-lucide-search'
+    }"
+    class="hover:bg-(--ui-bg) focus:bg-(--ui-bg) data-[state=open]:bg-(--ui-bg) min-w-3xs"
+    :ui="{
+      trailingIcon: 'group-data-[state=open]:rotate-180 transition-transform duration-200'
+    }"
+  />
+</template>
