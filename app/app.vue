6808f00c71bae12086d814f88afa00a29f9de56a
Louis F. <me@itslouis.dev>
2025-04-21 21:38:04 +0200

    feat(Pages): create default page & setup

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,38 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+const color = computed(() => colorMode.value === 'dark' ? '#1b1718' : '#ffffff')
+
+useHead({
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'theme-color', content: color }
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' }
+  ],
+  htmlAttrs: {
+    lang: 'fr'
+  }
+})
+
+const title = 'My ChatGPT'
+const description = 'My ChatGPT est une plateforme d\'IA conversationnelle avancée, alimentée par Gemini, conçue pour offrir des interactions intelligentes, réactives et naturelles. Idéal pour des applications personnelles ou professionnelles, My ChatGPT combine technologie de pointe et expérience utilisateur intuitive.'
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description
+})
+</script>
+
 <template>
-  <UApp>
-    <NuxtPage />
+  <UApp :toaster="{ position: 'top-right' }">
+    <NuxtLoadingIndicator color="var(--ui-primary)" />
+
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </UApp>
 </template>
